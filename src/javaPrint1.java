fc71af2c0c9eae09ae94f0b036723a6bc79ad43a
ManojPanuganti <manoj.panugantimanoj@gmail.com>
2017-01-25 16:27:39 +0530

    CHANGED

--- src/javaPrint1.java
+++ src/javaPrint1.java
@@ -2,6 +2,6 @@ public class javaPrint1 {
 
 	public static void main (String[] args)
 	{
-		System.out.println("Hi This is a Java program to be called from Jenkins");
+		System.out.println("Hi This is a EDITED Java program IN GITHUB");
 	}
 }
